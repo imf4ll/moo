2fdbf30b9e3b5ae0ecc7feb6ad0f2d9b29fd428d
z3oxs <z3ox1s@protonmail.com>
2023-04-28 21:35:29 -0300

    feat: Moved to monorepo

--- web/frontend/src/utils/time.ts
+++
@@ -1,6 +0,0 @@
-export const time = (duration: number) => {
-    const minutes = Math.floor(duration / 60);
-    const remaining = Math.floor(duration % 60);
-
-    return `${ minutes >= 10 ? minutes : '0' + minutes }:${ remaining >= 10 ? remaining : '0' + remaining }`;
-}
