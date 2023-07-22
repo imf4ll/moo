d2c518c8e431c09aefe3c904c6c6965f7edcdcb4
z3oxs <z3ox1s@protonmail.com>
2023-07-21 23:56:13 -0300

    fix: Removed unused dependency

--- web/backend/main.go
+++ web/backend/main.go
@@ -1,30 +1,17 @@
 package main
 
 import (
-    "log"
-    "os"
-    "fmt"
-
     "github.com/imf4ll/moo/backend/routes"
     
-    "github.com/joho/godotenv"
     "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Failed to load .env.");
-
-    }
-
-    port := os.Getenv("MOO_PORT");
-
     app := gin.Default();
     app.Use(cors.Default());
 
     routes.Routes(*app);
 
-    app.Run(fmt.Sprintf(":%s", port));
+    app.Run(":3001");
 }
